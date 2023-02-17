06f971e2e269784bd8f2fd6e8ab7bdcddd1f73bd
CarlosDavidAraujo <cdavidav19@gmail.com>
2023-02-17 10:11:39 -0300

    instalando a fonte new rocker

---
+++ src/Components/Button.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+const DefaultButton = styled.TouchableOpacity`
+  background-color: white;
+  border: none;
+  border-radius: 0;
+  border: 2px solid white;
+  font-size: 18px;
+  padding: 5px 10px;
+`;
+
+
+
+
+
+
+export { DefaultButton };
--- src/Screens/GameMenu.js
+++ src/Screens/GameMenu.js
@@ -1,22 +1,63 @@
 import React, { useContext } from "react";
 import styled from 'styled-components/native';
-import { TouchableOpacity, Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import bgImg from '../Images/menuImage.png';
+import { DefaultButton } from "../Components/Button";
+import AppLoading from 'expo-app-loading';
+import { useFonts, NewRocker_400Regular } from '@expo-google-fonts/new-rocker';
+
 
 const Container = styled.View`
-    height: 100vh;
-    background-color: black;
+    position: relative;
+    flex: 1;
+    background-color: white;
+`;
+
+const Background = styled.ImageBackground`
+    flex: 1;
+`;
+
+const StartButton = styled(DefaultButton)`
+    position: absolute;
+    bottom: 20%;
+    width: 50%;
+    align-self: center;
+    align-items: center;
+`;
+
+const DefaultText = styled.Text`
+    color: white;
+    font-size: 20px;
+    font-family: 'New Rocker', cursive;
 `;
 
 export default function GameMenu({ navigation }) {
-    return (
-        <Container>
-                <Text>Nightfall Village</Text>
-                <TouchableOpacity
-                    title="Iniciar Novo Jogo"
-                    onPress={() => navigation.navigate('DefinePlayers')}
-                />
-        </Container>
-    );
-}
+    let [fontsLoaded] = useFonts({
+        NewRocker_400Regular,
+    });
 
+    let fontSize = 24;
+    let paddingVertical = 6;
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <Container>
+                <Background source={bgImg} resizeMode={'cover'}>
+                    <Text>Nightfall Village</Text>
+                    <StartButton
+                        onPress={() => navigation.navigate('DefinePlayers')}
+                    >
+                        <Text style={{
+                            fontSize,
+                            paddingVertical,
+                            // Note the quoting of the value for `fontFamily` here; it expects a string!
+                            fontFamily: 'NewRocker_400Regular',
+                        }}>Iniciar novo jogo</Text>
+                    </StartButton>
+                </Background>
+            </Container>
+        );
+    }
+}
